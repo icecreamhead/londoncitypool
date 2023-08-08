36e837835a7bf38715f5697d0ae2859135f26568
Josh Cooke <icecreamhead@gmail.com>
2023-08-08 21:53:30 +0100

    test function

--- netlify/functions/hello.go
+++ netlify/functions/hello.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-  "github.com/aws/aws-lambda-go/events"
-  "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-  return &events.APIGatewayProxyResponse{
-    StatusCode:        200,
-    Body:              "Hello, World!",
-  }, nil
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, World!",
+	}, nil
 }
 
 func main() {
-  lambda.Start(handler)
+	lambda.Start(handler)
 }
-te
